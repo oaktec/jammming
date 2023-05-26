8e966c6948461f9af0ddff7c993d36703b66fd88
Ben Eatock <bdeatock@gmail.com>
2023-05-26 22:05:20 +0100

    Add save button

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -5,6 +5,10 @@ const Playlist = () => {
   return (
     <section className="Playlist result-block">
       <input className="playlist-name" placeholder="Enter Playlist Name" />
+
+      <button className="save-playlist-btn" disabled>
+        SAVE TO SPOTIFY
+      </button>
     </section>
   );
 };
