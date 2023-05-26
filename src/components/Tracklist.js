4b0b26ecf758f95e8736f3ff96f03666548dc820
Ben Eatock <bdeatock@gmail.com>
2023-05-26 21:55:08 +0100

    Add Tracklist

--- src/components/Tracklist.js
+++ src/components/Tracklist.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./css/Tracklist.css";
+import SearchResults from "./SearchResults";
 
 const Tracklist = () => {
-  return <section className="Tracklist result-block"></section>;
+  return (
+    <section className="Tracklist result-block">
+      <h2>Results</h2>
+      <SearchResults />
+    </section>
+  );
 };
 
 export default Tracklist;
