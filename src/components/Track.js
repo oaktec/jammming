f3def55dcd877556406e9010e73fbde304599c76
Ben Eatock <bdeatock@gmail.com>
2023-05-26 22:18:38 +0100

    Style tracks

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,10 +1,12 @@
 import React from "react";
 
+import "./css/Track.css";
+
 const Track = ({ track }) => {
   return (
     <div className="Track">
       <p>{track.name}</p>
-      <p>
+      <p className="subtext">
         {track.artist} | {track.album}
       </p>
     </div>
