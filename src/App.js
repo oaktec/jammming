42dc134cc76dd94cc5034e1a42bebbf45e341dd7
Ben Eatock <bdeatock@gmail.com>
2023-05-26 08:20:03 +0100

    Structure app jsx

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <header>
+        Ja<span>mmm</span>ing
+      </header>
+      <main></main>
+    </div>
+  );
 }
 
 export default App;
