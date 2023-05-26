6d0486f1901a176d96bd8464da1b4c8ce775b5f8
Ben Eatock <bdeatock@gmail.com>
2023-05-26 22:01:43 +0100

    Add Track component, and playlist name input

--- src/components/Playlist.js
+++ src/components/Playlist.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./css/Playlist.css";
 
 const Playlist = () => {
-  return <section className="Playlist result-block"></section>;
+  return (
+    <section className="Playlist result-block">
+      <input className="playlist-name" placeholder="Enter Playlist Name" />
+    </section>
+  );
 };
 
 export default Playlist;
