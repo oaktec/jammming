4207c7a5375553f37902ae5e76fbdec2bb205644
Ben Eatock <bdeatock@gmail.com>
2023-05-30 08:54:24 +0100

    Refactor spotifyService

--- src/services/spotifyService.js
+++ src/services/spotifyService.js
@@ -1,49 +1,43 @@
+const BASE_URL = "https://api.spotify.com";
+
 const Spotify = {
   _accessToken: null,
-  _expirationTime: null,
+  _tokenExpirationTime: null,
 
   async getAccessToken() {
-    if (this._accessToken) {
+    // If the access token is already set and has not expired, return early.
+    if (this._accessToken && Date.now() < this._tokenExpirationTime) {
       return;
     }
+
+    const clientId = process.env.REACT_APP_SPOTIFY_CLIENT_ID;
+    const clientSecret = process.env.REACT_APP_SPOTIFY_CLIENT_SECRET;
 
     const response = await fetch("https://accounts.spotify.com/api/token", {
       method: "POST",
       headers: {
         "Content-Type": "application/x-www-form-urlencoded",
       },
-      body: `grant_type=client_credentials&client_id=${process.env.REACT_APP_SPOTIFY_CLIENT_ID}&client_secret=${process.env.REACT_APP_SPOTIFY_CLIENT_SECRET}`,
+      body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
     });
 
     if (response.ok) {
       const { access_token } = await response.json();
       this._accessToken = access_token;
-      this._expirationTime = Date.now() + 3600 * 1000;
+      this._tokenExpirationTime = Date.now() + 3600 * 1000;
     } else {
       throw new Error("Request failed!");
     }
   },
-  checkAccessToken() {
-    if (!this._accessToken) return false;
-
-    if (Date.now() > this._expirationTime) {
-      this._accessToken = null;
-      return false;
-    }
-  },
   async search(term) {
-    if (!this.checkAccessToken()) {
-      await this.getAccessToken();
-    }
+    // Make sure there is a valid access token.
+    await this.getAccessToken();
 
-    const response = await fetch(
-      `https://api.spotify.com/v1/search?type=track&q=${term}`,
-      {
-        headers: {
-          Authorization: `Bearer ${this._accessToken}`,
-        },
-      }
-    );
+    const response = await fetch(`${BASE_URL}/v1/search?type=track&q=${term}`, {
+      headers: {
+        Authorization: `Bearer ${this._accessToken}`,
+      },
+    });
 
     if (response.ok) {
       const { tracks } = await response.json();
@@ -54,7 +48,7 @@ const Spotify = {
         album: track.album.name,
       }));
     } else {
-      throw new Error("Request failed!");
+      throw new Error(`Search request failed with status ${response.status}!`);
     }
   },
 };
