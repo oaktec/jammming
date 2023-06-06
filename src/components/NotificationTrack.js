e57d024cc427564f95c1ed29cfa876ff6232b54f
Ben Eatock <bdeatock@gmail.com>
2023-06-06 09:59:09 +0100

    Refactor NotificationTrack to use string literals

--- src/components/NotificationTrack.js
+++ src/components/NotificationTrack.js
@@ -3,11 +3,9 @@ import React from "react";
 import "./css/NotificationTrack.css";
 
 const NotificationTrack = ({ msg, type }) => {
-  const classes = "track-notification " + type;
-
   return (
     <div className="track">
-      <div className={classes}>
+      <div className={`track-notification ${type}`}>
         <p>{msg}</p>
       </div>
     </div>
