b23c399b69cdc0067df4b43959f7bc6386f1bebc
Ben Eatock <bdeatock@gmail.com>
2023-06-07 15:10:03 +0100

    Remove unused imports

--- src/components/TrackPreview/TrackPreview.js
+++ src/components/TrackPreview/TrackPreview.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React from "react";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faX } from "@fortawesome/free-solid-svg-icons";
