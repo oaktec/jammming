9be2ecba83c62cf6b9225eb6246d4a400a30397f
Ben Eatock <bdeatock@gmail.com>
2023-06-07 12:21:42 +0100

    Add buttons for close and mute

--- src/components/TrackPreview/TrackPreview.js
+++ src/components/TrackPreview/TrackPreview.js
@@ -1,11 +1,19 @@
 import React from "react";
 
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faVolumeHigh, faX } from "@fortawesome/free-solid-svg-icons";
+
 import "./TrackPreview.css";
 
 const TrackPreview = () => {
   return (
     <div className="track-preview">
-      <h3>Playing:</h3>
+      <button className="mute-btn">
+        <FontAwesomeIcon icon={faVolumeHigh} />
+      </button>
+      <button className="close-btn">
+        <FontAwesomeIcon icon={faX} />
+      </button>
       <div className="track-info">
         <p>Track Name</p>
         <p className="subtext">Artist | Album</p>
