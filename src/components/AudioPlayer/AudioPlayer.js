0a589b362510873855be4897cafe70512210e8c3
Ben Eatock <bdeatock@gmail.com>
2023-06-07 15:30:52 +0100

    Remove unused import

--- src/components/AudioPlayer/AudioPlayer.js
+++ src/components/AudioPlayer/AudioPlayer.js
@@ -1,4 +1,4 @@
-import React, { useRef, useEffect, useCallback } from "react";
+import React, { useRef, useEffect } from "react";
 import "./AudioPlayer.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
